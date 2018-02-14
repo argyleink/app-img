39614be7ac0bd178334ed46a780d8d21018d6bdb
Adam Argyle <atom@argyleink.com>
2018-02-13 22:14:16 -0800

    init working prototype

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# app-image
+# app-img (WIP)
+
+## Target API
+```html
+<app-img src='...'></app-img>
+<app-img src='...' viewport='#scrollcontainer'></app-img>
+<app-img src='...' no-shadow></app-img>
+<app-img src='...' style='--animation:none'></app-img>
+<app-img src='...' style='--fit:fill|contain|cover|none|scale-down'></app-img>
+<app-img src='...' style='--loading-bg:black;--loading-text_color:red;'></app-img>
+```
+
+```js
+$('app-img').addEventListener('loaded', ({target}) => 
+  console.log(target))
+```
---
+++ app-img.element.js
@@ -0,0 +1,98 @@
+const styles = `
+  <style>
+    :host {
+      position: relative;
+      display: inline-block;
+      contain: content;
+
+      --loading-bg: hsl(0,0%,85%);
+      --loading-text_color: hsl(0,0%,70%);
+      --fit: cover;
+      --fade-speed: 0.5s;
+      --animation: opacity var(--fade-speed) ease-out;
+    }
+
+    :host([loading]):after {
+      content: "Loading";
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      top: 0; left: 0; right: 0; bottom: 0;
+      color: var(--loading-text_color);
+      background: var(--loading-bg);
+      font-family: cursive;
+      z-index: -1;
+    }
+
+    :host([hidden]) { 
+      display: none; 
+    }
+
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+      object-fit: var(--fit);
+      border: none;
+
+      opacity: 1;
+      transition: var(--animation);
+    }
+
+    :host([loading]) img {
+      opacity: 0;
+    }
+  </style>
+`
+
+class AppImg extends HTMLElement {
+  constructor() {
+    super()
+
+    this.setAttribute('loading', '')
+    this.$img = document.createElement('img')
+
+    if (this.hasAttribute('no-shadow')) {
+      this.innerHTML += styles
+      this.appendChild(this.$img)
+    }
+    else {
+      this.$shadows = this.attachShadow({ mode: 'open' })
+      this.$shadows.innerHTML += styles
+      this.$shadows.appendChild(this.$img)
+    }
+
+    this.observer_viewport = this.hasAttribute('viewport') 
+    ? document.querySelector(this.getAttribute('viewport'))
+    : null
+  }
+
+  connectedCallback() {
+    this.observer = new IntersectionObserver(e => {
+      if (e[0].intersectionRatio < 1) return
+
+      this.$img.src = this.getAttribute('src')
+      this.observer.unobserve(this)
+    }, { 
+      root:       this.observer_viewport,
+      threshold:  1,
+      rootMargin: '0px',
+    })
+
+    this.observer.observe(this)
+
+    this.$img.onload = e => {
+      this.removeAttribute('loading')
+      this.dispatchEvent(new CustomEvent('loaded', { 
+        bubbles: false,
+        detail: { 
+          target: this, 
+          loaded: true,
+        },
+      }))
+    }
+  }
+}
+
+customElements.define('app-img', AppImg)
